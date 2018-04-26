521412681329e5159aeaedd1a3cc7fa021cf7e50
Sichen Liu <sl2947@cornell.edu>
2018-04-25 23:00:53 -0400

    Finish video stream

---
+++ video-processing/readme.md
@@ -0,0 +1,9 @@
+### image processing model 
+Front-end video stream to image process.
+Upload the image files to s3 bucket
+
+Package install
+```
+pip install opencv-python
+pip install boto3
+```
--- video-processing/video_process.py
+++ video-processing/video_process.py
@@ -1,13 +1,35 @@
 # Image processing by sample strategy.
 import cv2
-vidcap = cv2.VideoCapture('IMG_4944.mp4')
-success = True
-count = 0
-success, image = vidcap.read()
-while success:
-	vidcap.set(cv2.CAP_PROP_POS_MSEC, count * 1000)
-	cv2.imwrite("frame%d.jpg" % count, image)
-	success, image = vidcap.read()
-	count += 1
-	print(success)
+import glob
+import boto3
+import os 
 
+s3 = boto3.client('s3')
+
+def video_transfer(videoname):
+	# Transfer video to image.
+	vidcap = cv2.VideoCapture(videoname)
+	success = True
+	count = 0
+	success, image = vidcap.read()
+	while success:
+		# Sample rate: one frame per second.
+		vidcap.set(cv2.CAP_PROP_POS_MSEC, count * 1000)
+		cv2.imwrite("frame%d.jpg" % count, image)
+		success, image = vidcap.read()
+		count += 1
+		print(success)
+
+def upload_to_s3():
+	# Get the name of file 
+	filelist = glob.glob("*.jpg")
+	bucket_name = 'cs5412'
+	for fd in filelist:
+		s3.upload_file(fd, bucket_name, fd)
+	
+	# Delete all middle files 
+	os.system("rm frame*")
+
+if __name__ == "__main__":
+	video_transfer('IMG_4944.mp4')
+	upload_to_s3()
